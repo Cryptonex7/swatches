fb70ea2a9b5c61b95846842e76c730f76216806f
atharu1 <atharvu1@gmail.com>
2019-03-08 21:40:21 +0530

    Updates

---
+++ precache-manifest.4c498c22206f7706c2de831b1e0b081c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "461bcd6e2cc9d2ec6450",
+    "url": "/swatches/static/css/main.80e6d826.chunk.css"
+  },
+  {
+    "revision": "461bcd6e2cc9d2ec6450",
+    "url": "/swatches/static/js/main.461bcd6e.chunk.js"
+  },
+  {
+    "revision": "5cf501f7606d5cc9f61c",
+    "url": "/swatches/static/js/runtime~main.5cf501f7.js"
+  },
+  {
+    "revision": "c698a4e4af296e0c149f",
+    "url": "/swatches/static/js/2.c698a4e4.chunk.js"
+  },
+  {
+    "revision": "3cb5a675c1a9d5daa071a09261d35460",
+    "url": "/swatches/index.html"
+  }
+];
